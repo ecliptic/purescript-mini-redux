ebfca99cf13d17e488083546734c67b7bfd66d64
bkonkle <brandon@konkle.us>
2016-09-11 10:48:32 -0600

    Forget Babel for now

--- src/Redux/Mini.js
+++ src/Redux/Mini.js
@@ -1,7 +1,40 @@
-var ReduxMiniFF = require('./MiniFF')
+const ReactRedux = require('react-redux')
+const Redux = require('redux')
 
-exports.applyReducer = ReduxMiniFF.applyReducer
-exports.combineReducers = ReduxMiniFF.combineReducers
-exports.provider = ReduxMiniFF.provider
-exports.connect = ReduxMiniFF.connect
-exports.typeToString = ReduxMiniFF.typeToString
+exports.applyReducer = function (reducer) {
+  return function (action) {
+    return function (state) {
+      return reducer(action)(state)
+    }
+  }
+}
+
+exports.combineReducers = Redux.combineReducers
+
+exports.provider = ReactRedux.Provider
+
+exports.connect = function (mapStateToProps) {
+  return function (mapDispatchToProps) {
+    return ReactRedux.connect(mapStateToProps, mapDispatchToProps)
+  }
+}
+
+exports.typeToString = function (action) {
+  if (typeof action === 'string') return action
+
+  const name = action.constructor.name
+
+  const values = Object.keys(action).map(prop => {
+    if (prop.startsWith('value')) {
+      const value = action[prop]
+
+      if (typeof value === 'object') {
+        return '[object]'
+      }
+
+      return JSON.stringify(value)
+    }
+  })
+
+  return name + ' ' + values.join(' ')
+}
