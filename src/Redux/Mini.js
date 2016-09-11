38fbf205457e9900860512ac3dd540d5e03f9705
bkonkle <brandon@konkle.us>
2016-09-11 10:28:57 -0600

    Port the Redux library over from TodoMVC

---
+++ src/Redux/Mini.js
@@ -0,0 +1,7 @@
+var ReduxMiniFF = require('./MiniFF')
+
+exports.applyReducer = ReduxMiniFF.applyReducer
+exports.combineReducers = ReduxMiniFF.combineReducers
+exports.provider = ReduxMiniFF.provider
+exports.connect = ReduxMiniFF.connect
+exports.typeToString = ReduxMiniFF.typeToString
